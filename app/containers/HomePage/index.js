8828a0da0066c7d6f0f749142469a8cc3d807843
Radu <radu.chc@gmail.com>
2021-01-18 15:36:30 -0800

    Use class syntax.

--- app/containers/HomePage/index.js
+++ app/containers/HomePage/index.js
@@ -9,10 +9,15 @@ import React from 'react';
 import { FormattedMessage } from 'react-intl';
 import messages from './messages';
 
-export default function HomePage() {
-  return (
-    <h1>
-      <FormattedMessage {...messages.header} />
-    </h1>
-  );
+
+class HomePage extends React.Component {
+	render() {
+		return (
+		    <h1>
+		      <FormattedMessage {...messages.header} />
+		    </h1>
+		  );
+	}
 }
+
+export default HomePage
